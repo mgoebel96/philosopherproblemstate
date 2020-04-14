94731e41b4d447df162a62497894078c373c0919
m_goebel <murat.goebel@googlemail.com>
2020-04-14 17:15:42 +0200

    17:15 140420

--- src/main/java/de/netempire/PhilosophersDesk.java
+++ src/main/java/de/netempire/PhilosophersDesk.java
@@ -18,47 +18,46 @@ import static java.lang.Thread.sleep;
 public class PhilosophersDesk {
 
     private static String report;
+    static Fork fork1 = new Fork();
+    static Fork fork2 = new Fork();
+    static Fork fork3 = new Fork();
+    static Fork fork4 = new Fork();
+    static Fork fork5 = new Fork();
+    static Philosopher platon = new Philosopher("Platon", 1, fork1, fork2);
+    static Philosopher aristoteles = new Philosopher("Aristoteles",2, fork2, fork3);
+    static Philosopher herder = new Philosopher("Herder", 3,fork3, fork4);
+    static Philosopher fichte = new Philosopher("Fichte", 4,fork4, fork5);
+    static Philosopher schlegel = new Philosopher("Schlegel",5, fork5, fork1);
+    static Thread platonThread = new Thread(platon);
+    static Thread aristotelesThread = new Thread(aristoteles);
+    static Thread schlegelThread = new Thread(schlegel);
+    static Thread fichteThread = new Thread(fichte);
+    static Thread herderThread = new Thread(herder);
+    static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    static Thread controllerThread = new Thread();
+    static Philosopher[] philosophers = new Philosopher[5];
+    static Runnable controller;
+    static Date start = Calendar.getInstance().getTime();
 
     public static void main(String[] args) {
         PhilosophersDesk.startProcess();
     }
 
     private static void startProcess() {
-        Date start = Calendar.getInstance().getTime();
+        initialize();
+        start();
+    }
 
-        Fork fork1 = new Fork();
-        Fork fork2 = new Fork();
-        Fork fork3 = new Fork();
-        Fork fork4 = new Fork();
-        Fork fork5 = new Fork();
-
-        Philosopher platon = new Philosopher("Platon", 1, fork1, fork2);
+    public static void initialize(){
         platon.setEatingTime(750);
-        Philosopher aristoteles = new Philosopher("Aristoteles",2, fork2, fork3);
         aristoteles.setEatingTime(1000);
-        Philosopher herder = new Philosopher("Herder", 3,fork3, fork4);
         herder.setEatingTime(300);
-        Philosopher fichte = new Philosopher("Fichte", 4,fork4, fork5);
         fichte.setEatingTime(1500);
-        Philosopher schlegel = new Philosopher("Schlegel",5, fork5, fork1);
         schlegel.setEatingTime(500);
 
-        Philosopher[] philosophers = new Philosopher[]{platon, aristoteles, herder, fichte, schlegel};
+        philosophers = new Philosopher[]{platon, aristoteles, herder, fichte, schlegel};
 
-        Thread platonThread = new Thread(platon);
-        platonThread.start();
-        Thread aristotelesThread = new Thread(aristoteles);
-        aristotelesThread.start();
-        Thread schlegelThread = new Thread(schlegel);
-        schlegelThread.start();
-        Thread fichteThread = new Thread(fichte);
-        fichteThread.start();
-        Thread herderThread = new Thread(herder);
-        herderThread.start();
-
-        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-
-        Runnable controller = () -> {
+        controller = () -> {
             if(!platonThread.isAlive() && !herderThread.isAlive() && !aristotelesThread.isAlive() && !fichteThread.isAlive() && !schlegelThread.isAlive()){
                 platon.stop();
                 herder.stop();
@@ -99,13 +98,16 @@ public class PhilosophersDesk {
                 }
             }
         };
-        executor.scheduleAtFixedRate(controller, 0, 4, TimeUnit.SECONDS);
-        Thread controllerThread = new Thread(controller);
-        controllerThread.start();
-        System.out.println("ende");
     }
 
-    //todo Methoden
+    public static void start(){
+        platonThread.start();
+        aristotelesThread.start();
+        schlegelThread.start();
+        fichteThread.start();
+        herderThread.start();
+        executor.scheduleAtFixedRate(controller, 0, 4, TimeUnit.SECONDS);
+    }
 
     public static int computeDuration(Date to, Date from) {
         long difference = from.getTime() - to.getTime();
